457c214242319366ee3e6b9761c2c6fcd77c1598
JHOSEPH <jhoseph@localhost.localdomain>
2024-10-21 06:19:36 -0400

    fix: baseurl

--- app/api/api.ts
+++ app/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Configuración base de Axios
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000', // Endpoint base para todas las solicitudes
+  baseURL: 'https://web-production-a3a0.up.railway.app', // Endpoint base para todas las solicitudes
   headers: {
     'Content-Type': 'application/json', // Asegura que se envíe como JSON
   },
