910cb2a7773fe9383fd58efce423e86eaac3a555
JHOSEPH <jhoseph@localhost.localdomain>
2024-11-05 08:19:43 -0400

    add: article

--- app/landingpage/Article.tsx
+++ app/landingpage/Article.tsx
@@ -6,7 +6,9 @@ export default function Article() {
       <section className=" ">
         <div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
           <h1 className="text-4xl font-bold leading-none sm:text-5xl">
-          SOFTWARE: PLANTILLAS DE PRESION PLANTAR
+          SOFTWARE ANNISOFT
+          <br />
+          CONTROL DE PRECION DE LOS PIES
           </h1>
           <p className="px-8 mt-8 mb-12 text-lg">
             Cupiditate minima voluptate temporibus quia? Architecto beatae esse
