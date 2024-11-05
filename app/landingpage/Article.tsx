2c83f3fe424a0f220a96d4e38b77c6cadd5a49f2
JHOSEPH <jhoseph@localhost.localdomain>
2024-11-05 08:55:32 -0400

    add: message

--- app/landingpage/Article.tsx
+++ app/landingpage/Article.tsx
@@ -11,8 +11,7 @@ export default function Article() {
           CONTROL DE PRECION DE LOS PIES
           </h1>
           <p className="px-8 mt-8 mb-12 text-lg">
-            Cupiditate minima voluptate temporibus quia? Architecto beatae esse
-            ab amet vero eaque explicabo!
+          Descubre la importancia de cuidar cada paso que das con nuestro innovador software para registrar la presión de tus pies. Cada pisada cuenta, y con nuestra herramienta, podrás monitorizar y optimizar tu bienestar. ¡Camina con confianza hacia una vida más saludable y activa! Porque tus pies merecen el mejor cuidado, y nosotros estamos aquí para acompañarte en cada paso del camino. 🦶💙
           </p>
           <div className="flex flex-wrap justify-center">
             <Link href="/auth/formAdmin" className="px-8 py-3 m-2 text-lg font-semibold rounded border hover:bg-violet-600 ">
